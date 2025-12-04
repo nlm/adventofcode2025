93d930aad00e140b18d3634bc0b4c065ab86df6a
Nicolas Limage <nlimage@scaleway.com>
2025-12-04 11:55:42 +0100

    update internal lib

--- internal/math/math.go
+++ internal/math/math.go
@@ -4,13 +4,28 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// Number is a type constraint matching Integers and Floats.
 type Number interface {
 	constraints.Integer | constraints.Float
 }
 
+// Abs returns the absolute value of n.
 func Abs[T Number](n T) T {
 	if n < 0 {
 		return -n
 	}
 	return n
+}
+
+// Mod returns the positive modulus of 'n' by 'mod'.
+func Mod[T constraints.Integer](n T, mod T) T {
+	n %= mod
+	switch {
+	case n >= 0:
+		return n
+	case mod < 0:
+		return n - mod
+	default:
+		return n + mod
+	}
 }
--- internal/math/math_test.go
+++ internal/math/math_test.go
@@ -1,8 +1,11 @@
 package math
 
 import (
+	"fmt"
 	"math"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func BenchmarkStdAbs(b *testing.B) {
@@ -16,5 +19,25 @@ func BenchmarkAbs(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = Abs(i)
 		_ = Abs(-i)
+	}
+}
+
+func TestMod(t *testing.T) {
+	for _, tc := range []struct {
+		N   int
+		Mod int
+		Res int
+	}{
+		{1, 1, 0},
+		{10, 1, 0},
+		{1, 2, 1},
+		{10, 3, 1},
+		{-1, 2, 1},
+		{4, -2, 0},
+		{-1, -2, 1},
+	} {
+		t.Run(fmt.Sprint(tc), func(t *testing.T) {
+			assert.Equal(t, tc.Res, Mod(tc.N, tc.Mod))
+		})
 	}
 }
