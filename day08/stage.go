f2b72199d0d48af8398032c1606a09dc9ef2e5e4
Nicolas Limage <nlimage@scaleway.com>
2025-12-09 09:30:03 +0100

    day 08 improvements

--- day08/stage.go
+++ day08/stage.go
@@ -8,6 +8,7 @@ import (
 	"slices"
 	"strings"
 
+	"github.com/nlm/adventofcode2025/internal/combinations"
 	"github.com/nlm/adventofcode2025/internal/iterators"
 	"github.com/nlm/adventofcode2025/internal/sets"
 	"github.com/nlm/adventofcode2025/internal/stage"
@@ -32,44 +33,72 @@ func (b Box) Dist(b2 Box) float64 {
 	return math.Sqrt(math.Pow(float64(b.X()-b2.X()), 2) + math.Pow(float64(b.Y()-b2.Y()), 2) + math.Pow(float64(b.Z()-b2.Z()), 2))
 }
 
-type Circuits []sets.Set[Box]
+type Circuits struct {
+	sets  []sets.Set[Box]
+	boxes sets.Set[Box]
+}
+
+func NewCircuits(size int) *Circuits {
+	return &Circuits{
+		sets:  make([]sets.Set[Box], 0, size),
+		boxes: make(sets.Set[Box], size),
+	}
+}
+
+func (cs Circuits) String() string {
+	return fmt.Sprint(cs.sets)
+}
+
+func (cs Circuits) Circuits() []sets.Set[Box] {
+	return cs.sets
+}
 
 func (cs Circuits) Count() int {
-	return len(cs)
+	return len(cs.sets)
+}
+
+func (cs Circuits) FindSet(b Box) (int, sets.Set[Box]) {
+	if !cs.boxes.Contains(b) {
+		return -1, nil
+	}
+	for i, s := range cs.sets {
+		if s.Contains(b) {
+			return i, s
+		}
+	}
+	return -1, nil
 }
 
 func (cs *Circuits) Connect(b1, b2 Box) {
-	var b1Set, b2Set sets.Set[Box]
-	var b1Idx, b2Idx int
-	for i, c := range *cs {
-		if b1Set == nil && c.Contains(b1) {
-			b1Set = c
-			b1Idx = i
-		} else if b2Set == nil && c.Contains(b2) {
-			b2Set = c
-			b2Idx = i
-		}
-		if b1Set != nil && b2Set != nil {
-			break
-		}
+	b1Idx, b1Set := cs.FindSet(b1)
+	if b1Set != nil {
+		stage.Println("found set for:", b1)
 	}
-	_, _ = b1Idx, b2Idx
+	b2Idx, b2Set := cs.FindSet(b2)
+	if b2Set != nil {
+		stage.Println("found set for:", b2)
+	}
 	if b1Set == nil && b2Set == nil {
 		stage.Println("connect: all nil, new set")
 		newSet := make(sets.Set[Box])
 		newSet.Add(b1, b2)
-		*cs = append(*cs, newSet)
+		cs.sets = append(cs.sets, newSet)
+		cs.boxes.Add(b1, b2)
+	} else if b1Idx == b2Idx {
+		stage.Println("connect: same sets, do nothing")
 	} else if b1Set != nil && b2Set != nil {
 		stage.Println("connect: no nil, merge sets")
 		sets.Insert(b1Set, sets.Values(b2Set))
-		*cs = slices.Delete(*cs, b2Idx, b2Idx+1)
+		cs.sets = slices.Delete(cs.sets, b2Idx, b2Idx+1)
 		clear(b2Set)
 	} else if b1Set != nil {
 		stage.Println("connect: add", b2, "to existing", b1, "set")
 		b1Set.Add(b2)
+		cs.boxes.Add(b2)
 	} else if b2Set != nil {
 		stage.Println("connect: add", b1, "to existing", b2, "set")
 		b2Set.Add(b1)
+		cs.boxes.Add(b1)
 	} else {
 		panic("internal error")
 	}
@@ -77,20 +106,13 @@ func (cs *Circuits) Connect(b1, b2 Box) {
 
 func CalculateDistances(boxes []Box) map[float64]*[2]Box {
 	dists := make(map[float64]*[2]Box)
-	for _, b1 := range boxes {
-		for _, b2 := range boxes {
-			if b1 == b2 {
-				continue
-			}
-			dist := b1.Dist(b2)
-			if dists[dist] != nil {
-				if *dists[dist] != [2]Box{b2, b1} {
-					panic("duplicate")
-				}
-				continue
-			}
-			dists[dist] = &[2]Box{b1, b2}
+	for pair := range combinations.Combinations(boxes, 2) {
+		b1, b2 := pair[0], pair[1]
+		dist := b1.Dist(b2)
+		if dists[dist] != nil {
+			panic("duplicate")
 		}
+		dists[dist] = &[2]Box{b1, b2}
 	}
 	return dists
 }
@@ -118,20 +140,22 @@ func Stage1(input io.Reader) (any, error) {
 	// 	1: {{1, 1, 1}, {2, 2, 2}},
 	// 	2: {{3, 3, 3}, {4, 4, 4}},
 	// 	3: {{5, 5, 5}, {4, 4, 4}},
+	// 	4: {{5, 5, 5}, {1, 1, 1}},
 	// }
-	circuits := make(Circuits, 0)
+
+	circuits := NewCircuits(0)
 	iterations := 0
 	for _, k := range slices.Sorted(maps.Keys(dists)) {
-		stage.Println("handling:", k, dists[k])
+		stage.Println("handling:", k, *dists[k])
 		circuits.Connect((*dists[k])[0], (*dists[k])[1])
-		stage.Println("circuits after:", circuits)
+		stage.Println("circuits after:", *circuits)
 		iterations++
 		if !(iterations < maxIterations) {
 			break
 		}
 		stage.Println()
 	}
-	circuitLens := iterators.MapSlice(circuits, func(s sets.Set[Box]) int {
+	circuitLens := iterators.MapSlice(circuits.Circuits(), func(s sets.Set[Box]) int {
 		return len(s)
 	})
 	slices.SortFunc(circuitLens, func(a, b int) int {
@@ -151,13 +175,13 @@ func Stage1(input io.Reader) (any, error) {
 func Stage2(input io.Reader) (any, error) {
 	boxes := ParseBoxes(input)
 	dists := CalculateDistances(boxes)
-	circuits := make(Circuits, 0)
+	circuits := NewCircuits(0)
 	for _, k := range slices.Sorted(maps.Keys(dists)) {
 		stage.Println("handling:", "dist=", k, "set=", dists[k])
 		circuits.Connect((*dists[k])[0], (*dists[k])[1])
-		// stage.Println("circuits after:", circuits)
-		stage.Println("CHECK:", len(boxes), len(circuits[0]))
-		if len(boxes) == len(circuits[0]) {
+		stage.Println("circuits after:", circuits)
+		// stage.Println("CHECK:", len(boxes), len(circuits.Circuits()[0]))
+		if len(boxes) == len(circuits.Circuits()[0]) {
 			b1, b2 := (*dists[k])[0], (*dists[k])[1]
 			stage.Println("Final CNX:", b1, b2, "->", b1.X()*b2.X())
 			return b1.X() * b2.X(), nil
